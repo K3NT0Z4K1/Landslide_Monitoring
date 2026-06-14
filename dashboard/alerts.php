6b612ea659c7851c945a488a2c8ab7cd3f66c29a
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-06-14 18:46:14 +0800

    Added a function in app js, and added other script in alerts

--- dashboard/alerts.php
+++ dashboard/alerts.php
@@ -120,6 +120,7 @@ $active_page   = 'alerts';
   </div>
 </div>
 
+<script src="../assets/js/app.js"></script>
 <script>
 setInterval(() => {
   document.getElementById('clock').textContent = new Date().toTimeString().slice(0,8);
